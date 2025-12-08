6a7a88d78de66a2d7b25eb40ebd251daf22a95e6
Ankit53 <formicro32@gmail.com>
2025-12-08 12:30:35 +0530

    feat: Add call count column to admin

--- core/admin.py
+++ core/admin.py
@@ -3,10 +3,14 @@ from .models import Contact, Feedback
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'phone', 'email', 'project_type', 'created_at')
+    list_display = ('name', 'phone', 'email', 'project_type', 'call_count', 'created_at')
     list_filter = ('project_type', 'preferred_time', 'created_at')
     search_fields = ('name', 'phone', 'email', 'message')
     readonly_fields = ('created_at',)
+
+    def call_count(self, obj):
+        return Contact.objects.filter(name=obj.name).count()
+    call_count.short_description = 'Number of Requests'
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
